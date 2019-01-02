8d7b32f0468456d6ef082f89bd2ceb9c68ee5e5b
scann <apple.eater01@gmail.com>
2019-01-02 15:55:07 +0200

    Login Form with Form and field components

--- source/components/LoginForm.js
+++ source/components/LoginForm.js
@@ -8,6 +8,51 @@ import Styles from './styles.m.css';
 
 export class LoginForm extends Component {
     render() {
-        return 'Добро пожаловать!';
+        const { touched, isSubmitting, errors, values } = this.props;
+
+        const invalidEmailStyle = cx({
+            [ Styles.invalidInput ]: touched.email && errors.email,
+        });
+
+        const invalidPasswordStyle = cx({
+            [ Styles.invalidInput ]: touched.password && errors.password,
+        });
+
+        const disabledButtonStyle = cx({
+            [ Styles.disabledButton ]: isSubmitting,
+        })
+
+        const submittingMessage = isSubmitting ? 'Вход...' : 'Войти';
+
+        return (
+            <Form>
+                <Field
+                    className = { invalidEmailStyle }
+                    name = 'email'
+                    placeholder = 'Почта'
+                    type = 'email'
+                />
+                <Field
+                    className = { invalidPasswordStyle }
+                    name = 'password'
+                    placeholder = 'Пароль'
+                    type = 'password'
+                />
+                <label>
+                    <Field
+                        checked = { values.remember }
+                        name = 'remember'
+                        type = 'checkbox'
+                    />
+                    <span>Запомнить меня</span>
+                </label>
+                <button
+                    className = { disabledButtonStyle }
+                    disabled = { isSubmitting }
+                    type = 'submit'>
+                    { submittingMessage }
+                </button>
+            </Form>
+        );
     }
 }
