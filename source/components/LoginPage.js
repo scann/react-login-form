3267fe6fa5d3e634b6c50d2ae199e20a920bac5a
scann <apple.eater01@gmail.com>
2019-01-02 15:55:33 +0200

    Login Page with Formik component and validation

--- source/components/LoginPage.js
+++ source/components/LoginPage.js
@@ -12,10 +12,46 @@ import { LoginForm } from './LoginForm';
 
 @hot(module)
 export class LoginPage extends Component {
+    state = {
+        isAuthenticated: false,
+    };
+
+    _login = async(credentials, actions) => {
+        await delay(1000);
+
+        console.log(JSON.stringify(credentials, null, 4));
+
+        this.setState({
+            isAuthenticated: true,
+        });
+
+        actions.setSubmitting(false);
+        await delay(5000);
+
+        this.setState({
+            isAuthenticated: false,
+        });
+    };
+
     render() {
+        const { isAuthenticated } = this.state;
+
         return (
-            <section className={Styles.loginPage}>
-                <LoginForm />
+            <section className = { Styles.loginPage }>
+                { isAuthenticated && (
+                    <div className = { Styles.loginMessage }>Добро пожаловать!</div>
+                )}
+                <Formik
+                    initialValues = {{
+                        email:    'test@email.com',
+                        password: '12345',
+                        remember: false,
+
+                    }}
+                    render = { (props) => <LoginForm { ...props }/> }
+                    validationSchema = { schema }
+                    onSubmit = { this._login }
+                />
             </section>
         );
     }
